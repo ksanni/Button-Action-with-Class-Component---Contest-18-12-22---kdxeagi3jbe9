73994fa2cb647f77e5802ce5a6debf41d733da96
ksanni <ksanni58627>
2022-12-18 19:58:43 +0530

    update2

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 function App(){
-	const [displayMessage, setDisplayMessage] = useState (false);
-	return (
+	const [displayMessage, setDisplayMessage]=useState(false);
+	return(
 		<div id="main">
-           {
-			(displayMessage===false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tools.</p>)
-		   }
-		   	<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
+			{
+				(displayMessage===false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+			}
+
+			<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
 		</div>
 	);
 }
